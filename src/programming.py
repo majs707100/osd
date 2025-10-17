df391a54f0303805adc82467c52d971afe24b56a
majs707100 <majs707100@gmail.com>
2025-10-17 17:24:16 +0900

    modify

---
+++ src/programming.py
@@ -0,0 +1,23 @@
+
+a = 15
+b = 4
+
+print(f"--- 사칙 연산 결과 (a={a}, b={b}) ---")
+result_add = a + b
+print(f"덧셈 (a + b): {result_add}")
+
+result_sub = a - b
+print(f"뺄셈 (a - b): {result_sub}")
+
+result_mul = a * b
+print(f"곱셈 (a * b): {result_mul}")
+
+
+result_div = a / b
+print(f"나눗셈 (a / b): {result_div}")
+
+result_div_int = a // b
+print(f"정수 나눗셈 (a // b): {result_div_int}")
+
+result_mod = a % b
+print(f"나머지 연산 (a % b): {result_mod}")
